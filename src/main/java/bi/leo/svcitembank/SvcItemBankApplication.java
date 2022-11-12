9915a1eb07edc6623ae0a7bf947d0799d96bea66
Leo Bi <biminglei@gmail.com>
2022-11-12 19:41:31 +0800

    init submit

---
+++ src/main/java/bi/leo/svcitembank/SvcItemBankApplication.java
@@ -0,0 +1,13 @@
+package bi.leo.svcitembank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcItemBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcItemBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bi/leo/svcitembank/SvcItemBankApplicationTests.java
@@ -0,0 +1,13 @@
+package bi.leo.svcitembank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcItemBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
